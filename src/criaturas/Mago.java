d844dad3539c1b02761a1ee17df09db0a12173bd
Jdiosa12 <Juanchom04@gmail.com>
2025-04-11 19:51:08 -0500

    Segundo commit

--- src/criaturas/Mago.java
+++ src/criaturas/Mago.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package criaturas;
 
-/**
- *
- * @author j3157
- */
-public class Mago {
+
+public interface Mago {
+    
+    void lanzarHechizo();
+    void aprenderHechizo();
+    
     
 }
--- src/criaturas/Volador.java
+++ src/criaturas/Volador.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package criaturas;
 
-/**
- *
- * @author j3157
- */
-public class Volador {
+public interface Volador {
+    
+    void volar();
+    void aterrizar();
+    
     
 }
