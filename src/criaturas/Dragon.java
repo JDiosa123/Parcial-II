7b1942c7308f716a31fdae8f2f56518773c3c04f
Brayan Zamora <zamorabrayan0722@gmail.com>
2025-04-12 19:25:54 -0500

    Correciones de errores y creacion de la clase Main

--- src/criaturas/Dragon.java
+++ src/criaturas/Dragon.java
@@ -1,6 +1,6 @@
 
 package criaturas;
-
+import Arma.Arma;
 
 public class Dragon extends Criaturas implements Volador {
 
@@ -15,24 +15,24 @@ public class Dragon extends Criaturas implements Volador {
             arma.atacarConArma();
             daño += arma.getDañoAdicional();
         }
-        System.out.println(nombre + " ataca con fuego causando " + daño + " de daño.");
+        System.out.println(nombre + " ataca con fuego AZUL causando " + daño + " de Impacto.");
         objetivo.defender(daño);
     }
 
     @Override
     public void defender(int daño) {
         salud -= daño;
-        System.out.println(nombre + " recibió " + daño + " de daño. Salud restante: " + salud);
+        System.out.println(nombre + " recibio " + daño + " de Impacto. Salud restante: " + salud);
     }
 
     @Override
     public void volar() {
-        System.out.println(nombre + " está volando.");
+        System.out.println(nombre + " esta volando.");
     }
 
     @Override
     public void aterrizar() {
-        System.out.println(nombre + " aterrizó.");
+        System.out.println(nombre + " aterrizo.");
     }
 
 }
--- src/criaturas/Main.java
+++ src/criaturas/Main.java
@@ -1,23 +1,24 @@
-package Criaturas;
+package criaturas;
+import Arma.Arma;
 
 public class Main {
     public static void main(String[] args) {
         
-        Arma espada = new Arma("Espada de Acero", 10);
-        Arma baston = new Arma("Bastón Mágico", 15);
-        Arma garras = new Arma("Garras de Fuego", 20);
+        Arma espada = new Arma("Espada de diamante", 10);
+        Arma baston = new Arma("Barita Magica", 15);
+        Arma garras = new Arma("Garras de Fuego AZUL", 20);
 
         
-        Guerrero guerrero = new Guerrero("Leonidas", 100, 20);
-        Mago mago = new Mago("Merlín", 80, 15);
-        Dragon dragon = new Dragon("Smaug", 150, 25);
+        Guerrero guerrero = new Guerrero("Steve", 100, 20);
+        Mago mago = new Mago("Gandalf", 80, 15);
+        Dragon dragon = new Dragon("Fionx", 150, 25);
 
         
         guerrero.equiparArma(espada);
         mago.equiparArma(baston);
         dragon.equiparArma(garras);
 
-        System.out.println("\n⚔️ ¡Comienza la batalla!\n");
+        System.out.println("\n️¡Comienza la batallaAaAa!\n");
 
         
         guerrero.atacar(dragon);
@@ -29,7 +30,7 @@ public class Main {
 
         guerrero.atacar(mago);
 
-        System.out.println("\n🔚 Estado final:");
+        System.out.println("\n Estado final:");
         System.out.println(guerrero.getNombre() + " salud: " + guerrero.getSalud());
         System.out.println(mago.getNombre() + " salud: " + mago.getSalud());
         System.out.println(dragon.getNombre() + " salud: " + dragon.getSalud());
